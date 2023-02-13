da40bc01b946b056d1862129c991740a36012d92
HliasMpGH <115589434+HliasMpGH@users.noreply.github.com>
2023-02-13 21:30:14 +0200

    progress

--- Matrix.java
+++ Matrix.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Matrix {
 	Scanner in = new Scanner(System.in);
@@ -8,11 +9,11 @@ public class Matrix {
 	
 	public void temp() {
 		System.out.print("Insert the number of variables that will be used: ");
-		 vars = in.nextInt();
-		 System.out.print("and the number of the constraints: ");
-		 consts = in.nextInt();
+		vars = in.nextInt();
+		System.out.print("and the number of the constraints: ");
+		consts = in.nextInt();
 		// structure the matrix based on the constrains and variables
-		 mat = new int[consts + 1][vars * 2 + 1]; // add 1 to both dimensions for the objective function
+		mat = new int[consts + 1][vars * 2 + 1]; // add 1 to both dimensions for the objective function
 	}
 	public static void main(String[] args) {
 		Matrix m = new Matrix();
@@ -21,17 +22,26 @@ public class Matrix {
 	}
 
 	private void createMatrix() {
-		int j = 0;
-		in.nextLine();
-		//for (int i = 0; i <= consts; i++) {
+		String[] coef;
+		in.nextLine(); 
+		// edw prwta tha gemisw thn teleytaia grammh(obj synarthsh)
+		for (int i = 0; i < consts; i++) {
+			int j = 0;
 			System.out.println("Enter the first constraint:");
 			String con = in.nextLine();
-			String[] temp = con.split("['*'||'+'||'-']+"); // place the constraint coefficients in the simplex matrix
-			for (String var : temp)
-				System.out.println(var);
-			//mat[i][j] =
-			j++;
-		//}
+			coef = con.split("[\\W]*['*'][\\W]*\\w[\\d]+[\\W]*"); // place the constraint coefficients in the simplex matrix
+			for (String var : coef) {
+				mat[i][j] = Integer.valueOf(var);
+				j++;
+			}
+			mat[i][vars + i] = 1; // 1 for the surplus variables
+		}
+
+		for (int[] var : mat) {
+			for (int var2 : var) {
+				System.out.println(var2);
+			}
+		}
 		
 	}
 	
