cb1d019205d4bb86b4d86c04b1a73c0519f4dc7a
HliasMpGH <t8210226@aueb.gr>
2023-02-19 21:52:34 +0200

    OF regex progress

--- Matrix.java
+++ Matrix.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Matrix {
 	static Scanner in = new Scanner(System.in);
@@ -13,6 +12,12 @@ public class Matrix {
 		setMatrix();
 	}
 
+	Matrix() {
+		this.vars = 1;
+		this.consts = 1;
+		setMatrix();
+	}
+
 	// structures the matrix based on the constrains and variables
 	private void setMatrix() {
 		mat = new int[consts + 1][vars + consts + 2]; // add 1 to both dimensions for the objective function(add 1 more collumn for the values of the constaints)
@@ -22,12 +27,12 @@ public class Matrix {
 		String[] coef;
 		in.nextLine();
 		// edw prwta tha gemisw thn teleytaia grammh(obj synarthsh)
-		//setObjFunction();
+		setObjFunction();
 		for (int i = 0; i < consts; i++) {
 			System.out.println("Enter constraint number " + (i + 1) +":");
 			String con = in.nextLine();
 			coef = con.split("[\\W]*['*'][\\W]*\\w[\\d]+[\\W]*"); // get the constraint coefficients & values
-			mat[i][vars + consts + 1] = Integer.valueOf(coef[coef.length - 1]); // fill the last collumn of the simplex matrix with the constrain values
+			mat[i][vars + consts + 1] = Integer.valueOf(coef[coef.length - 1]); // fill the last collumn of the simplex matrix with the constraint values
 			for (int j = 0; j < coef.length - 1; j++) {
 				mat[i][j] = Integer.valueOf(coef[j]); // place the coefficients in the matrix
 			}
@@ -36,8 +41,9 @@ public class Matrix {
 		
 		for (int[] var : mat) {
 			for (int var2 : var) {
-				System.out.println(var2);
+				System.out.print(var2 + "\t");
 			}
+			System.out.println();
 		}
 		
 
@@ -47,34 +53,22 @@ public class Matrix {
 	public void setObjFunction() {
 		System.out.print("Insert the objective function: ");
 		String of = in.nextLine();
-		// [\\W]*['*'][\\W]*\\w[\\d]+[\\W]*
-		// [Z|z][\\W]*['='][\\W]*['*'][\\W]*\\w[\\d]+[\\W]*
-		/*
-		  TO DO:
-		   regex gia obj function
-		*/
-		String[] coef = of.split("[Zz][\\W]*=[\\W]*['*'][\\W]*\\w[\\d]+[\\W]");
-		for (String var : coef) {
-			System.out.println(var);
+		// provlima : an exei kena anamesa stous telestes to regex split dhmiourgei provlima (tha exei ena keno meta apo ton arithmo)
+		String[] coef = of.split("[Z|z][\\W]*['='][\\W]*|['*'][\\W]*\\w[\\d]+[\\W]*"); // get the OF coefficients
+		for (int j = 1; j < coef.length; j++) {
+			mat[mat.length - 1][j - 1] = -Integer.valueOf(coef[j]); // place the coefficients in the matrix(negatives)
 		}
+		mat[mat.length - 1][vars + consts] = 1; // 1 for the Z coefficent
 	}
-	Matrix() {
-
-	}
-
 
 	// main, will be on Decision class
 	public static void main(String[] args) {
-		/*
 		System.out.print("Insert the number of variables that will be used: ");
 		int vars = in.nextInt();
 		System.out.print("and the number of the constraints: ");
 		int consts = in.nextInt();
-		*/
-		Matrix m = new Matrix();
-		m.setObjFunction();
-		//m.setConstraints();
-	//	m.fillMatrix();
+		Matrix m = new Matrix(vars, consts);
+		m.fillMatrix();
 	}
 
 
